17ff8aabc2eb2db8732035068e0750bb9c7a68ca
Rebecca Sundel <sundelrp@gmail.com>
2021-10-02 14:48:58 -0400

    iteration 2-7

---
+++ src/components/CreditCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './CreditCard.css'
+
+ const CreditCard = (props) => {
+   const number = props.number;
+   const ccNumber = number.substring(number.length - 4);
+   const ccStyle = {
+     backgroundColor: props.bgColor,
+     color: props.color,
+   };
+   return (
+     <div style={ccStyle} className="border-box creditcard">
+       <h4>• • • • • • • • • • • {ccNumber}</h4>
+       <p>
+         expires: {props.expirationMonth}/{props.expirationYear}
+       </p>
+       <p>{props.owner}</p>
+       <p>{props.bank}</p>
+     </div>
+   );
+ };
+
+ export default CreditCard; 
